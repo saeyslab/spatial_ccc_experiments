bb5ac34e0e86fc19ee53a880f9406e80e805b734
FrancescaDr <francesca.drummer@hpc-build01.scidom.de>
2024-06-10 17:58:14 +0200

    preprocess allen brain data

--- scripts/allen_brain_atlas/preprocess_allen_brain_atlas.py
+++ scripts/allen_brain_atlas/preprocess_allen_brain_atlas.py
@@ -15,12 +15,18 @@ adata = sc.read_h5ad(f"{path}raw/Zhuang-ABCA-1-raw.h5ad")
 df_cell = pd.read_csv(f"{path}raw/cell_metadata.csv", index_col=0)
 df_gene = pd.read_csv(f"{path}raw/gene_metadata.csv", index_col=0)
 
+adata.var = df_gene
+
 print("...adding annotation to adata.obs")
 adata.obs = adata.obs.join(df_cell, rsuffix="_right")
 adata = adata[adata.obs.brain_section_label_right.notna()]
 
+df_cell_sub = df_cell[df_cell['brain_section_label'] == 'Zhuang-ABCA-1.080']
+
+adata = adata[adata.obs.index.isin(list(df_cell_sub.index))]
+
 print("...adding data collection schema to adata.obs")
-adata.obs["dataset_id"] = "yao_2023"
+adata.obs["dataset_id"] = "Zhuang_2023"
 adata.obs["dataset_id"] = adata.obs["dataset_id"].astype("category")
 adata.obs["sample_id"] = adata.obs["feature_matrix_label"].astype("category")
 adata.obs["donor_id"] = adata.obs["donor_label"].astype("category")
@@ -44,10 +50,6 @@ adata.obs["celltype"] = adata.obs["class"].astype("category")
 adata.obs["fov"] = adata.obs["brain_section_label"].astype("category")
 
 adata.obsm["spatial"] = np.array(adata.obs[["x", "y"]])
-
-if sys.argv[2]:
-    print(f"Subet adata on slice section {sys.argv[2]}")
-    adata 
 
 print(f"Writing processed anndata object to '{out_path}/processed/'")
 adata.write(f"{path}processed/yao_2023_merfish_brain.h5ad")
