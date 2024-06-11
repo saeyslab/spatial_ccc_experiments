18a25047db648739e8d544e1ae0328a0f8cad761
Robrecht Cannoodt <rcannood@gmail.com>
2024-06-11 10:20:49 +0200

    add example python method

---
+++ src/methods/example_python/script.py
@@ -0,0 +1,24 @@
+import anndata as ad
+import pandas as pd
+
+## VIASH START
+par = {
+  "input": "resources/datasets/Zhuang_2023_merfish_brain/dataset.h5ad",
+  "output": "resources/results/example_python/prediction.h5ad"
+}
+## VIASH END
+
+adata = ad.read_h5ad(par["input"])
+
+# generate prediction
+ccc_pred = pd.DataFrame(
+  # ...
+)
+
+# create output
+output = ad.AnnData(
+  uns={
+    "ccc_pred": ccc_pred
+  }
+)
+output.write_h5ad(par["output"], compression = "gzip")
